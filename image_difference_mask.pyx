82151a7aa5bc04561e10891e4bd0cfaae7609610
ver228 <ver228@gmail.com>
2015-07-21 11:28:08 +0100

    .

--- image_difference_mask.pyx
+++ image_difference_mask.pyx
@@ -15,19 +15,24 @@ cdef inline int imAbsDiff(unsigned char a, unsigned char b):
     return a-b if a>b else b-a 
 @cython.boundscheck(False)
 
-def image_difference(np.ndarray[np.uint8_t, ndim=2] f, np.ndarray[np.uint8_t, ndim=2] g):
+def image_difference_mask(np.ndarray[np.uint8_t, ndim=2] f, np.ndarray[np.uint8_t, ndim=2] g):
     #f, g are to one dim vector
     
     cdef int n_row = f.shape[0];
     cdef int n_col = f.shape[1];
     cdef int i, k
     cdef double total;
-    #cdef double tot_pix = 0;
+    cdef double tot_pix = 0;
     
     for i in range(n_row):
         for j in range(n_col):
-            total += <double>imAbsDiff(f[i,j],g[i,j])
-            
+            if ( f[i,j] != 0 ) and ( g[i,j] != 0 ):
+                total += <double>imAbsDiff(f[i,j],g[i,j])
+                tot_pix += 1;
+        
     #print tot_pix
-    return total/<double>f.size;
+    if tot_pix > 0:
+        return total/tot_pix;
+    else:
+        return -1;
     
